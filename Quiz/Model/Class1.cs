40109fca723c1f2acf89c7bcf2dadc62e94e191b
wsluzalek <sluzalek.weronika@gmail.com>
2025-04-14 10:23:32 +0200

    zmieniaaam

---
+++ Quiz/Model/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Model
+{
+    public class Class1
+    {
+        int id;
+
+        public Class1() { }
+    }
+}
