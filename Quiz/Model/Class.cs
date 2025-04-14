000bb193e5d6e12bc81166d854700c60572e3c9e
conta <conta@LAPTOP-KINGA>
2025-04-14 10:06:41 +0200

    tworze nowa klase

---
+++ Quiz/Model/Class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Model
+{
+    internal class Class
+    {
+        int wiek;
+        public Class(int wiek1) 
+        { 
+            wiek = wiek1;
+        }
+    }
+}
