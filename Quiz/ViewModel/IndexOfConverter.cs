6c12ec85369520647309c4a06fce67826c4c4332
conta <conta@LAPTOP-KINGA>
2025-05-06 14:12:22 +0200

    dodałam funkcjonalość tworzenia nowego quizu, ale narazie nie ma wszystkich opcji, ale przynajmniej cokolwiek działa, dlatego commituje

---
+++ Quiz/ViewModel/IndexOfConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Quiz.ViewModel
+{
+    public class IndexOfConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var item = value;
+            var collection = parameter as System.Collections.IList;
+
+            if (collection != null)
+            {
+                return collection.IndexOf(item);
+            }
+
+            return -1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
+
--- Quiz/ViewModel/QuizEditorViewModel.cs
+++ Quiz/ViewModel/QuizEditorViewModel.cs
@@ -2,44 +2,121 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
+using System.IO;
 
 namespace Quiz.ViewModel
 {
-    public class QuizEditorViewModel
+    public class QuizEditorViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<Question> Questions { get; set; } = new();
-        public string QuizName { get; set; } = "Nowy Quiz";
-        public string Password { get; set; } = ""; // tymczasowo na sztywno
+        public event PropertyChangedEventHandler PropertyChanged;
 
-        public ICommand SaveCommand { get; }
-        public ICommand LoadCommand { get; }
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _quizName = "Nowy Quiz";
+        public string QuizName
+        {
+            get => _quizName;
+            set
+            {
+                if (_quizName != value)
+                {
+                    _quizName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ObservableCollection<Question> Questions { get; set; } = new();
+        public ObservableCollection<Model.Quiz> Quizzes { get; } = new(); // Fully qualify the Quiz type
+
+        private Model.Quiz _selectedQuiz; // Fully qualify the Quiz type
+        public Model.Quiz SelectedQuiz
+        {
+            get => _selectedQuiz;
+            set
+            {
+                _selectedQuiz = value;
+                OnPropertyChanged();
+                if (_selectedQuiz != null)
+                {
+                    QuizName = _selectedQuiz.Name;
+                    Questions.Clear();
+                    foreach (var q in _selectedQuiz.Questions)
+                        Questions.Add(q);
+                }
+            }
+        }
+
+        public ICommand AddQuestionCommand { get; }
+        public ICommand SaveQuizCommand { get; }
+        public ICommand LoadQuizCommand { get; }
+        public ICommand CreateNewQuizCommand { get; }
 
         public QuizEditorViewModel()
         {
-            SaveCommand = new RelayCommand(_ => Save());
-            LoadCommand = new RelayCommand(_ => Load());
+            AddQuestionCommand = new RelayCommand(_ => AddQuestion());
+            SaveQuizCommand = new RelayCommand(_ => SaveQuiz());
+            LoadQuizCommand = new RelayCommand(_ => LoadQuizzes());
+            CreateNewQuizCommand = new RelayCommand(_ => CreateNewQuiz());
         }
 
-        private void Save()
+        private void AddQuestion()
         {
-            var quiz = new Quiz.Model.Quiz
+            Questions.Add(new Question
+            {
+                Text = "Nowe pytanie",
+                Answers = new List<Answer> // Changed from ObservableCollection to List
+                {
+                    new Answer { Text = "Odpowiedź 1", IsCorrect = false },
+                    new Answer { Text = "Odpowiedź 2", IsCorrect = false },
+                    new Answer { Text = "Odpowiedź 3", IsCorrect = false },
+                    new Answer { Text = "Odpowiedź 4", IsCorrect = false }
+                }
+            });
+        }
+
+        private void SaveQuiz()
+        {
+            var quiz = new Model.Quiz // Fully qualify the Quiz type
             {
                 Name = QuizName,
-                Questions = Questions.ToList()
+                Questions = Questions.ToList() // Convert ObservableCollection to List
             };
-            QuizSerializer.SaveEncrypted($"{QuizName}.quiz", quiz, Password);
+
+            Quizzes.Add(quiz);
+
+            string json = System.Text.Json.JsonSerializer.Serialize(quiz);
+            File.WriteAllText($"{quiz.Name}.json", json);
         }
 
-        private void Load()
+        private void LoadQuizzes()
         {
-            var loaded = QuizSerializer.LoadEncrypted($"{QuizName}.quiz", Password);
+            Quizzes.Clear();
+            var files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.json");
+            foreach (var file in files)
+            {
+                var json = File.ReadAllText(file);
+                var quiz = System.Text.Json.JsonSerializer.Deserialize<Model.Quiz>(json); // Fully qualify the Quiz type
+                if (quiz != null)
+                    Quizzes.Add(quiz);
+            }
+        }
+
+        private void CreateNewQuiz()
+        {
+            QuizName = "Nowy Quiz";
             Questions.Clear();
-            foreach (var q in loaded.Questions)
-                Questions.Add(q);
         }
     }
 }
